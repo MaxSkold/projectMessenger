6fc46a298bb35980b245a95dd177bd7fbfac538d
maxskold <mrk642004@gmail.com>
2025-04-14 00:14:17 +0200

    day 2

--- main.go
+++ main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/MaxSkold/projectMessenger/internal/config"
 	"log"
 )
 
-func main() {
-	dbconfig, err := config.NewDBConfig()
+func connectDB() (*sql.DB, error) {
+	dbConfig, err := config.NewDBConfig()
 	if err != nil {
-		log.Fatalf("Error loading DB config: %v", err)
+		return nil, fmt.Errorf("error loading DB config: %v", err)
 	}
 
-	db, err := dbconfig.Connect()
+	db, err := dbConfig.Connect()
 	if err != nil {
-		log.Fatalf("Database connection error: %v", err)
+		return nil, fmt.Errorf("critical database connection error: %v", err)
 	}
-	defer db.Close()
+
+	// Возвращаем базу данных для дальнейшего использования
+	return db, nil
+}
+
+func main() {
+	// Подключаемся к базе данных
+	db, err := connectDB()
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	// Используем defer для безопасного закрытия соединения
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatalf("Error closing database connection: %v", err)
+		} else {
+			log.Printf("Database connection closed successfully")
+		}
+	}()
 
 	log.Println("App is running")
 }
